0e92998cc0a7f528df2cb8cdf0c18808f2d62ea3
jinseoplee <jjjp9966@gmail.com>
2026-03-04 18:18:09 +0900

    Initial commit

---
+++ src/test/java/io/github/jinseoplee/CouponIssuanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jinseoplee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponIssuanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
